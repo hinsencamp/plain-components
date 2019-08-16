c0bad73d4c4abaf250b8df1608fcbb2a67742109
Andreas Remdt <andreas.remdt@gmail.com>
2019-08-16 09:23:15 +0200

    add alternative button api

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -23,25 +23,59 @@ export default class Button extends React.Component {
   };
 
   render() {
-    const { className: classNameString, isFocused, ...props } = this.props;
-
-    const classes = !classNameString
+    /*const classes = !classNameString
       ? ""
       : classNameString
           .split(" ")
           .map(className => {
             return theme[className] ? theme[className] : className;
           })
-          .join(" ");
+          .join(" ");*/
+
+    const { isFocused, ...props } = this.props;
+    const classes = getClassNames(theme, "button");
+    const appliedClasses = applyClassNames(classes, props, styles.default);
 
     return (
       <button
         ref={this.buttonRef}
-        className={classes === "" ? styles.default : classes}
         {...props}
+        className={appliedClasses}
       >
         {this.props.children}
       </button>
     );
   }
+}
+
+function getClassNames(theme, namespace) {
+  const matches = {};
+  
+  for (let selector in theme) {
+    if (selector.includes(namespace)) {
+      matches[selector] = theme[selector];
+    }
+  }
+
+  return matches;
+}
+
+function applyClassNames(classes, props, defaultStyles) {
+  let matches = [];
+
+  for (let selector in classes) {
+    if (Object.values(props).includes(selector.replace("button-", ""))) {
+      matches.push(classes[selector]);
+    }
+  }
+
+  if (props.className) {
+    matches.push(props.className);
+  }
+
+  if (matches.length > 0) {
+    return matches.join(" ");
+  }
+
+  return defaultStyles || null;
 }
